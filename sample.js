ba8627629cf6cdd9488908028e3c7cbbadad1652
mwangox <32011741+mwangox@users.noreply.github.com>
2020-12-13 12:09:42 +0300

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,8 +1,15 @@
-var Logger           = require("./logger")
-var logger           = new Logger("lib/sample.js")
+var Logger = require("./logger")
+var logger = new Logger("lib/sample.js")
 
 logger.info("Hello Javascript")
 logger.debug("Hello %s", "Golang")
 
 var p_language = "Java"
 logger.warn("Hello ${p_language}")
+
+/*
+---------------- Output --------------------------------
+2020-12-13 12:07:21 [info] lib/sample.js Hello Javascript
+2020-12-13 12:07:21 [debug] lib/sample.js Hello Golang
+2020-12-13 12:07:21 [warn] lib/sample.js Hello Java
+*/
